37a1cea0ee8f0573c21b2b288b626a3d17b197df
Adrien BOURGOIS <a.bourgois@student.isartdigital.com>
2016-11-21 11:25:02 +0100

    Add additionals files

---
+++ Source/GladiatorGame/GladiatorGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GladiatorGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLADIATORGAME_API AGladiatorGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
