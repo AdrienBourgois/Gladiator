7d790b0e39e2f86cec66ba109034d0448e0c49fa
bastien teche <b.teche@student.isartdigital.com>
2016-12-07 10:05:01 +0100

    Create server in gameInstance but client not find it

--- Source/GladiatorGame/GladiatorGame.h
+++ Source/GladiatorGame/GladiatorGame.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "Engine.h"
+#include "UnrealNetwork.h"
+#include "Online.h"
 #include "Runtime/UMG/Public/UMG.h"
 #include "Runtime/UMG/Public/UMGStyle.h"
 #include "Runtime/UMG/Public/Slate/SObjectWidget.h"
