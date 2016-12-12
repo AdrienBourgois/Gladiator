aeb48edd17d4779269a0b1fc603ed752ab814934
bastien teche <b.teche@student.isartdigital.com>
2016-12-12 15:11:33 +0100

    Change networking Blueprint to C++, possibility tu create server and client connect to server

--- Source/GladiatorGame/GladiatorGame.Build.cs
+++ Source/GladiatorGame/GladiatorGame.Build.cs
@@ -7,10 +7,10 @@ public class GladiatorGame : ModuleRules
 	public GladiatorGame(TargetInfo Target)
 	{
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "UMG", "Slate", "SlateCore"});
-        //PublicDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "OnlineSubsystemUtils"});
+        PublicDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "OnlineSubsystemUtils"});
 
         PrivateDependencyModuleNames.AddRange(new string[] { "AIModule", "GameplayTasks"});
-        //    DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
+        DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
 
         //if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
         //{
